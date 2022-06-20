9f75dc7cdfe14bccd34d6c5f9b0f869e1a4dd820
leez4rd <101297800+leez4rd@users.noreply.github.com>
2022-06-19 20:28:23 -0400

    fixed some stuff
    win conditions still not working, but player cycling, board display, etc all works for arbitrary board sizes, player numbers, etc

--- gomoku.py
+++ gomoku.py
@@ -35,44 +35,59 @@ import random
 
 
 
+""" 
+current issues / missing features
+
+the game does not end when a full row is created 
+the symbol does not change from turn to turn 
+we only have two symbols now, should be pulled from random character set 
+win checks not tested yet 
+when bad input is entered, we need to catch the exception instead of quitting the program 
+need to determine best design choices and optimize code
+
+"""
+
+
+
 # important architectural decision:
 # do we want to track the evolution of a Board object to check wins
 # or do we want each player to keep track of their own moves so we can just iterate through array ? 
 
 class Board: 
 	def __init__(self, m, n, k):
-		rows = m 
-		columns = n
-		
+		self.rows = m 
+		self.columns = n
 		# initialize empty gameboard 
-		gameboard = [[' ']*m for _ in range(n)]
+		self.gameboard = [[' ']*m for _ in range(n)]
 
 	def update_board(self, move, character):
-		gameboard[move[0]move[1]] = character 
+		self.gameboard[move[0]][move[1]] = character
+ 
 
 	# only works for two players right now 
 	def display_board(self):
 		for i in range(self.rows):
 			for j in range(self.columns):
-				if gameboard[i][j] == 'X':
+				if self.gameboard[i][j] == 'X':
 					tile = 'X'
-				elif gameboard[i][j] == 'O':
+				elif self.gameboard[i][j] == 'O':
 					tile = 'O'
 				else:
 					tile = ' '
 				print("|", tile, " |", sep ='', end ='')
 			print('\n', end='')
 
+
 class Player:
 
 	def __init__(self, st):
-		name = st
+		self.name = st
 		x = random.randint(0, 1)
 		if x == 1:
-			char = 'X'
+			self.char = 'X'
 		else:
-			char = 'O'
-		moves = [] 
+			self.char = 'O'
+		self.moves = [] 
 
 	def record_move(self, move):
 		self.moves += [move]
@@ -82,15 +97,17 @@ class Game:
 
 	def __init__(self, rows, columns, k, *args):
 
-		m = rows
-		n = columns
-		victory_length = k
-		gameboard = Board(rows, columns, k)
-		moves = {}
+		self.m = rows
+		self.n = columns
+		self.victory_length = k
+		self.gameboard = Board(rows, columns, k)
+		self.moves = {}
 
 		# list of names
-		names = [args[i] for i in range(len(args))]
-		player_dict = {}
+		self.number_of_players = len(args)
+		self.names = [args[i] for i in range(len(args))]
+		self.player_dict = {}
+		self.turn_count = 0 # keep track of turn number for player cycle 
 		# maybe add input checking to generate default names if none provided? 
 		# something like
 
@@ -99,31 +116,40 @@ class Game:
 			# P1 = Player('Mario')
 			# P2 = Player('Luigi')
 			# may need to do it this way...
-			player_dict['P1'] = Player('Mario')
-			player_dict['P2'] = Player('Luigi')
+			self.player_dict['P1'] = Player('Mario')
+			self.player_dict['P2'] = Player('Luigi')
 		else:
 			# initialize players with names 
-			for i, name in enumerate(names):
+			for i, name in enumerate(self.names):
 				pnum = 'P' + str(i + 1) 
 				# setattr(self, pnum, Player(name))
-				player_dict[pnum] = Player(name)
+				self.player_dict[pnum] = Player(name)
 
-		current_player = player_dict['P1'] # might be problematic
-	
+		self.current_player = self.player_dict['P1'] # might be problematic
+
 	def run_game(self):
 		game_over = False 
 
 		while not game_over:
 			# ask current player for a move 
-			gameboard.display_board()
+			self.gameboard.display_board()
 			move = list(map(int, input("Enter a set of coordinates separated by a comma: ").split(',')))
 
-			if gameboard.gameboard[move[0]][moves[1]] == ' ':
+			if self.gameboard.gameboard[move[0]][move[1]] == ' ':
 				self.current_player.record_move(move)
-				self.gameboard.update_board(move, current_player.char)
+				self.gameboard.update_board(move, self.current_player.char)
+				if self.turn_count == self.m * self.n: # careful of off by one errors here 
+					game_over = True 
+					winner = None 
+				if self.find_wins():
+					game_over = True 
+					winner = self.current_player
+				
 				self.next_turn() # need a way to update player 
+
 			else:
-				print("That space already contains a pebble")
+				print("That space already contains a pebble") # don't change player, just return to loop
+
 			'''
 			#if space is open...
 			if (gameboard[move[0]][move[1]] == ' '):
@@ -147,8 +173,14 @@ class Game:
 			else:
 				print("that space already contains a pebble")
 			'''
+		if winner == None:
+			print("It's a tie")
+		else:
+			print("The winner is ", winner.name)
 
 	def next_turn(self):
+		self.turn_count += 1 # increment the turn count 
+		self.current_player = self.player_dict['P' + str(self.turn_count % self.number_of_players + 1)]
 		# how do we change players when the list of variable names for players is created dynamically?
 		# can we create a dynamic dictionary as well or instead? 	
 		# get key for value of current player, then add 1 to integer part of key to get new key
@@ -162,24 +194,39 @@ class Game:
 			return False
 
 	def diagonals(self):
+		return False # temporary escape until I fix this function 
 		possible_wins = [] # maybe keep track of all groups of k - 1 to expedite this instead of checking each time
 		# then we only need to check which ones were previously one away from victory
 		# this will be unique for each player, so may be memory-intensive 
 
-		possible_win_num = len(self.current_player.moves) // self.k
+		possible_win_num = int(len(self.current_player.moves) / self.victory_length)
 
 		# this should create a bunch of lists of possible win rows 
-		for i in range(len(possible_win_num)):
-			possible_win_rows += self.current_player.moves[i:i+k]
+		possible_win_rows = []
+		for i in range(possible_win_num):
+			possible_win_rows += self.current_player.moves[i:i+self.victory_length]
 
 		start = 0
-		while consecutive < self.k and start < len(possible_win_rows):
+		consecutive = 0
+		# inefficient because of overlap between possible win rows, maybe just check discrete chunks and merge? 
+		# need to think about algorithm here 
+		while consecutive < self.victory_length and start < len(possible_win_rows):
+			print(start)
+			print(possible_win_rows)
 
-			if possible_win_rows[start][0] + 1 == possible_win_rows[start + 1][0]:
-				if possible_win_rows[start][1] + 1 == possible_win_rows[start + 1][1]:
-					consecutive += 1
+			# WHY DOES CHANGING THE INDEXING MAKE IT ONLY DISPLAY ONE SYMBOL !???
+			# does not work at edges... 
+			if start == possible_win_num:
+				return False # check this logic 
+			else:
+				if possible_win_rows[start + 1][0] + 1 == possible_win_rows[start + 1][0]:
+					if possible_win_rows[start + 1][1] + 1 == possible_win_rows[start + 1][1]:
+						consecutive += 1
+					else:
+						consecutive = 0
 			start += 1
-		if consecutive >= self.k:
+
+		if consecutive >= self.victory_length:
 
 			return True # should I just set self.win = True or do that using return value from function ? 
 		else:
@@ -201,19 +248,23 @@ class Game:
 		possible_wins = [] # maybe keep track of all groups of k - 1 to expedite this instead of checking each time
 		# then we only need to check which ones were previously one away from victory
 		# this will be unique for each player, so may be memory-intensive 
-
-		possible_win_num = len(self.current_player.moves) // self.k
+		possible_win_rows = []
+		possible_win_num = int(len(self.current_player.moves) / self.victory_length)
 
 		# this should create a bunch of lists of possible win rows 
-		for i in range(len(possible_win_num)):
-			possible_win_rows += self.current_player.moves[i:i+k][0]
+		for i in range(possible_win_num):
+			possible_win_rows += self.current_player.moves[i:i+self.victory_length][0]
 
 		start = 0
-		while consecutive < self.k and start < len(possible_win_rows):
+		consecutive = 0
+		# PROBLEM: NEED TO CHECK EQUALITY IE THAT WE ARE IN SAME ROW OR WE WILL GET SPURIOUS WINS 
+		while consecutive < self.victory_length and start < len(possible_win_rows):
 			if possible_win_rows[start] + 1 == possible_win_rows[start + 1]:
 				consecutive += 1
+			else:
+				consecutive = 0
 			start += 1
-		if consecutive >= self.k:
+		if consecutive >= self.victory_length:
 			return True # should I just set self.win = True or do that using return value from function ? 
 		else:
 			return False 
@@ -227,19 +278,22 @@ class Game:
 		possible_wins = [] # maybe keep track of all groups of k - 1 to expedite this instead of checking each time
 		# then we only need to check which ones were previously one away from victory
 		# this will be unique for each player, so may be memory-intensive 
-
-		possible_win_num = len(self.current_player.moves) // self.k
+		possible_win_rows = []
+		possible_win_num = int(len(self.current_player.moves) / self.victory_length)
 
 		# this should create a bunch of lists of possible win rows 
-		for i in range(len(possible_win_num)):
-			possible_win_rows += self.current_player.moves[i:i+k][1]
+		for i in range(possible_win_num):
+			possible_win_rows += self.current_player.moves[i:i+self.victory_length][1]
 
 		start = 0
-		while consecutive < self.k and start < len(possible_win_rows):
+		consecutive = 0
+		while consecutive < self.victory_length and start < len(possible_win_rows):
 			if possible_win_rows[start] + 1 == possible_win_rows[start + 1]:
 				consecutive += 1
+			else:
+				consecutive = 0
 			start += 1
-		if consecutive >= self.k:
+		if consecutive >= self.victory_length:
 			return True # should I just set self.win = True or do that using return value from function ? 
 		else:
 			return False 
@@ -265,8 +319,8 @@ class Game:
 
 
 def main():
-	tictactoe = game(3, 3, 3, "me", "you")
-	gomoku = game(10, 10, 5, "me", "you")
+	tictactoe = Game(3, 3, 3, "me", "you")
+	gomoku = Game(10, 10, 5, "me", "you")
 
 	#GUI
 
@@ -284,7 +338,7 @@ def main():
 	text_target_ip.pack()
 
 	# buttons
-	btn_listen = tk.Button(window, text = "Start Game", width=50, command = game.run_game())
+	btn_listen = tk.Button(window, text = "Start Game", width=50, command = gomoku.run_game())
 	btn_listen.pack(anchor = tk.CENTER, expand = True)
 
 	window.mainloop()
